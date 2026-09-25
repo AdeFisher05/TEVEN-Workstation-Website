6ac9420b658c6c64ab407fffe248c2661a55d51c
Aderinola Fisher <fisheraderinola@gmail.com>
2026-09-25 21:46:18 +0100

    Registration form added

---
+++ auth.js
@@ -0,0 +1,16 @@
+const register = document.getElementById("register");
+const signIn = document.getElementById("sign-in");
+const memberAuth = document.querySelector(".member-auth");
+const userAuth = document.querySelector(".user-auth");
+register.addEventListener("click", () => {
+    signIn.classList.remove("active");
+    register.classList.add("active");
+    memberAuth.style.display = "none";
+    userAuth.style.display = "block";
+});
+signIn.addEventListener("click", () => {
+    register.classList.remove("active");
+    signIn.classList.add("active");
+    userAuth.style.display = "none";
+    memberAuth.style.display = "block";
+})
--- script.js
+++ script.js
@@ -4,3 +4,18 @@ menu?.addEventListener('click', () => {
   links?.classList.toggle('open');
   menu.setAttribute('aria-expanded', String(links?.classList.contains('open')));
 });
+localStorage.setItem(
+  "tevenUser",
+  JSON.stringify({
+    name: "Fisher Aderinola",
+    email: "fisher@example.com",
+  }),
+);
+const user = JSON.parse(localStorage.getItem("tevenUser"));
+
+const params = new URLSearchParams(window.location.search);
+const workspace = params.get("workspace");
+
+if (!user) {
+  window.location.href = `auth.html?redirect=booking&workspace=${workspace}`;
+}
